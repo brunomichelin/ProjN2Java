f3a4aa7651e98bf3f64a373eef13bcd9a4f672ff
BrunoHenriqueMicheli <BrunoHenriqueMicheli@DESKTOP-4MHF9P8>
2018-10-02 19:47:30 -0300

    Envio

---
+++ FTT javaFX sceneBuilder/src/principal/Principal.java
@@ -0,0 +1,86 @@
+package principal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println(new Date());
+		
+		/*
+		People p = new People(100, "ZeSilva");
+		
+		System.out.println(p);
+		
+		
+		String connectionString =
+				"jdbc:sqlserver://localhost:1433;" +
+				"database=db_java;" +
+				"user=sa;" +
+				"password=;";
+		
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection(connectionString);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println(connection.getCatalog());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			Statement stm = connection.createStatement();
+			
+			ResultSet rs = stm.executeQuery("SELECT * FROM PEOPLE;");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("ID"));
+				System.out.println(" - " + rs.getString("NAME"));
+			}
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		*/
+		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root;
+		
+		try {
+			root = (BorderPane)FXMLLoader.load(getClass().getResource("telaOne.fxml"));
+			
+			Scene scene = new Scene(root, 800, 600);
+			
+			scene.getStylesheets().add(getClass().getResource("telaOne.css").toExternalForm());
+			
+			primaryStage.setTitle("Menu Principal");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ FTT javaFX sceneBuilder/src/principal/telaFuncionariosController.java
@@ -0,0 +1,48 @@
+package principal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class telaFuncionariosController {
+
+	@FXML //Annotation
+	private Label labelStatus;
+	@FXML
+	private Button btnIncluir;
+	@FXML
+	private Button btnAlterar;
+	@FXML
+	private Button btnConsultar;
+	@FXML
+	private Button btnExcluir;
+	
+	@FXML
+	protected void handlerOpenFileButton(ActionEvent event) {
+		System.out.println("Vai corinthians!");
+		labelStatus.setText("Vai Corinthians!!!");
+		
+	}
+	
+	@FXML
+	protected void btnIncluir_OnClick(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	protected void btnAlterar_OnClick(ActionEvent event) {
+
+	}
+	
+	@FXML
+	protected void btnConsultar_OnClick(ActionEvent event) {
+
+	}
+	
+	@FXML
+	protected void btnExcluir_OnClick(ActionEvent event) {
+
+	}
+	
+}
